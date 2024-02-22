9063eb0ee6edab40638cd87985e370de3fad9565
Joseph <joseph.clemente@hacktiv.io>
2024-02-22 13:04:33 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls, } from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+import {Suspense, useState} from "react";
+import Model from "./components/Shoe";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [mesh, setMesh] = useState("#ffffff");
+    const [strips, setStrips] = useState("#ffffff");
+    const [soul, setSoul] = useState("#ffffff");
+    
+    return (
+        <div class="wrapper">
+            <div class="card">
+                <div class="product-canvas">
+                  <Canvas>
+                    <Suspense fallback={null}>
+                      <ambientLight/>
+                      <spotLight intensity={0.9} angle={0.1} penumbra={1} position={[10,15,10]} castShadow/>
+                      <OrbitControls enablePan enableZoom enableRotate/>
+                      <Model customColors={{mesh, strips, soul}} />
+                    </Suspense>
+                  </Canvas>
+                </div>
+                <h2>Color picker</h2>
+                <div class='colors'>
+                    <div>
+                        <input type="color" id="head" name="head" value="#ffffff" onChange={(e) => setMesh(e.target.value)}/>
+                        <label for="head">Main</label>
+                    </div>
+
+                    <div>
+                        <input type="color" id="body" name="body" value="#ffffff" onChange={(e) => setStrips(e.target.value)}/>
+                        <label for="body">Stripes</label>
+                    </div>
+                    <div>
+                        <input type="color" id="body" name="body" value="#ffffff" onChange={(e) => setSoul(e.target.value)}/>
+                        <label for="body">Soul</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
